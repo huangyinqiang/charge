2d09ab808cc4b91b4fca223b87ddc4076455d49c
zhengkun <xjtu_zhengkun@126.com>
2018-08-07 00:42:29 +0800

    添加手机验证码填写和验证

--- charge-weixin/src/main/java/net/inconnection/charge/weixin/controller/DeviceController.java
+++ charge-weixin/src/main/java/net/inconnection/charge/weixin/controller/DeviceController.java
@@ -26,9 +26,15 @@ public class DeviceController extends Controller {
             log.info("处理URL类型的二维码成功，qr＝" + qr);
         }
 
-        HnKejueResponse json = deviceService.queryDeviceByQrNum(qr);
-        log.info("根据二维码查询设备结束：" + json);
-        this.renderJson(json);
+        if(qr.length() == 9){
+            HnKejueResponse json = deviceService.queryDeviceByQrNum(qr);
+            log.info("根据二维码查询设备结束：" + json);
+            this.renderJson(json);
+        }else {
+            //TODO 处理新设备
+        }
+
+
     }
 
     public void queryDeviceInfoByDeviceId() {
