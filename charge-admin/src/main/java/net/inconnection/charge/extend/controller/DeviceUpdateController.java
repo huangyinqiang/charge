a7584233dd8f2fd112483b90178207e385617655
zhengkun <xjtu_zhengkun@126.com>
2018-08-12 14:01:24 +0800

    整理设备升级的消息队列处理的activeMq

--- charge-admin/src/main/java/net/inconnection/charge/extend/controller/DeviceUpdateController.java
+++ charge-admin/src/main/java/net/inconnection/charge/extend/controller/DeviceUpdateController.java
@@ -2,15 +2,12 @@ package net.inconnection.charge.extend.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import net.inconnection.charge.admin.common.base.BaseController;
-import net.inconnection.charge.extend.chargeDevice.jms.ActiveMQMsgClient;
-import net.inconnection.charge.extend.chargeDevice.utils.RedisUtil;
+import net.inconnection.charge.extend.chargeDevice.jms.DeviceUpdateMQProcessor;
 import net.inconnection.charge.extend.chargeDevice.utils.SEQGeneration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -77,19 +74,19 @@ public class DeviceUpdateController extends BaseController {
         json.put("seq",seq);
         json.put("timeStr",timeStr);
 
-        ActiveMQMsgClient client = new ActiveMQMsgClient();
+        DeviceUpdateMQProcessor deviceUpdateMQProcessor = new DeviceUpdateMQProcessor();
         try {
-            client.start();
+            deviceUpdateMQProcessor.start();
             _log.info("JMSCorrelationID"+key);
-            client.request(key,json.toJSONString());
+            deviceUpdateMQProcessor.request(key,json.toJSONString());
         } catch (Exception e) {
-            _log.error("activeMQ客户端启动错误!",e);
+            _log.error("DeviceUpdateMQProcessor 客户端启动错误!",e);
         }
 
         //阻塞并从队列等待反馈
-        String status = client.onMessage(TIME_OUT);
+        String status = deviceUpdateMQProcessor.onMessage(TIME_OUT);
         try {
-            client.stop();
+            deviceUpdateMQProcessor.stop();
         } catch (Exception e) {
             _log.error("activeMQ客户端停止错误!",e);
         }
