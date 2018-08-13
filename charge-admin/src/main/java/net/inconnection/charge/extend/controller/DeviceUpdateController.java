c46b93eb2767984ae878cd241a1b815597905d09
huangyinqiang <13227718060@163.com>
2018-08-14 06:17:52 +0800

    测试完成文件上传

--- charge-admin/src/main/java/net/inconnection/charge/extend/controller/DeviceUpdateController.java
+++ charge-admin/src/main/java/net/inconnection/charge/extend/controller/DeviceUpdateController.java
@@ -1,18 +1,22 @@
 package net.inconnection.charge.extend.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.jfinal.plugin.activerecord.Record;
+import com.jfinal.upload.UploadFile;
+import net.inconnection.charge.admin.common.DBTool;
 import net.inconnection.charge.admin.common.base.BaseController;
+import net.inconnection.charge.admin.common.util.StringUtil;
 import net.inconnection.charge.extend.chargeDevice.jms.DeviceUpdateMQClient;
 import net.inconnection.charge.extend.chargeDevice.utils.RedisUtil;
 import net.inconnection.charge.extend.chargeDevice.utils.SEQGeneration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import static net.inconnection.charge.extend.chargeDevice.protocol.ProtocolConstant.MQTT_TOPIC_CUR_VERSION;
 import static net.inconnection.charge.extend.chargeDevice.protocol.ProtocolConstant.MQTT_TOPIC_INDUSTRY_CHARGE;
@@ -26,17 +30,48 @@ public class DeviceUpdateController extends BaseController {
 
     public void listPage() {
 
+        setAttr("powerStationId","222");
         render("UpdateDevice.html");
     }
 
 
-/*    public void updateDeviceData(File file, String gwId) {
+    public void listData() {
+        System.out.println("sssssssssssssssssssssssssssssssss");
+        Object[] queryParams = this.getQueryParams();
+        String[] properties = (String[])queryParams[0];
+        String[] symbols = (String[])queryParams[1];
+        Object[] values = (Object[])queryParams[2];
+        String orderBy = this.getOrderBy();
+        if (StringUtil.isEmpty(orderBy)) {
+            orderBy = "update_time desc";
+        }
+
+        List<Record> list = DBTool.findByMultPropertiesDbSource("zcurd_busi", "yc_charge_pile", properties, symbols, values, orderBy, this.getPager());
+        this.renderDatagrid(list, DBTool.countByMultPropertiesDbSource("zcurd_busi", "yc_charge_pile", properties, symbols, values));
+    }
+
+
+    public void updateDeviceData() {
+        File file = getFiles().get(0).getFile();
+        String fileName = file.getName();
+
         String industry = "CHARGE";
         String protocolVersion = "1";
-        String keyStr = industry + "/" + protocolVersion + "/" + gwId;
+        String keyStr = industry + "/" + protocolVersion + "/" + fileName;
         byte[] buff = null;
         try {
-            buff = file.getBytes();
+            //buff = file.getBytes();
+            FileInputStream fis = new FileInputStream(file);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            byte[] b = new byte[1024];
+            int n;
+            while ((n = fis.read(b)) != -1)
+            {
+                bos.write(b, 0, n);
+            }
+            fis.close();
+            bos.close();
+            buff = bos.toByteArray();
         } catch (IOException e) {
             _log.error("文件获取byte数组错误!",e);
         }
@@ -46,12 +81,12 @@ public class DeviceUpdateController extends BaseController {
         } catch (UnsupportedEncodingException e) {
             _log.error("字符编码转换错误!",e);
         }
-        boolean flag = RedisUtil.getJedis().exists(key);
-        if(!flag){
-            RedisUtil.set(key,buff,60*10);
-        }
+        //boolean flag = RedisUtil.getJedis().exists(key);
+        //if(!flag){
+            RedisUtil.set(key,buff,60*20);
+       // }
         renderText("OK");
-    }*/
+    }
 
     public void updateDeviceProcess(String gwid , String filename) {
         long start = System.currentTimeMillis();
