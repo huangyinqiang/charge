4cbb53a762ccb49e76b151772b3dac311d16e31e
gnaioo <gnaioo@qq.com>
2018-10-17 14:41:57 +0800

    老设备充值优惠

--- charge-weixin/src/main/java/net/inconnection/charge/weixin/model/DeviceProject.java
+++ charge-weixin/src/main/java/net/inconnection/charge/weixin/model/DeviceProject.java
@@ -6,9 +6,17 @@ import com.jfinal.plugin.activerecord.Model;
 public class DeviceProject extends Model<DeviceProject> {
     private static final long serialVersionUID = 918424281313379941L;
     private static final Log log = Log.getLog(DeviceProject.class);
-    public static final DeviceProject dao = new DeviceProject();
+    public static final DeviceProject me = new DeviceProject();
 
     public DeviceProject() {
+    }
+
+    public DeviceProject queryProjectIdByDeviceId(String deviceId) {
+        log.info("根据deviceId查询活动信息:deviceId=" + deviceId);
+        DeviceProject deviceProject = this.findFirst("select * from yc_device_project where device_id = ?" ,
+                new Object[]{deviceId});
+        log.info("根据deviceId查询活动信息:" + deviceProject);
+        return deviceProject;
     }
 }
 
